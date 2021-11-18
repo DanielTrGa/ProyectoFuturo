2411c91de3f6e147e4c5aaad3f1548218e557c95
DanielTrGa <danieltriguerogalvez@hotmail.com>
2021-11-18 23:39:09 +0100

    commit final

--- src/FuturoProyecto.java
+++ src/FuturoProyecto.java
@@ -1,5 +1,5 @@
 public class FuturoProyecto {
-
+//Daniel Triguero
     public static void main(String[] args) {
         System.out.println("Estudiante " + args[0] + ": " + veredicto (args[1], args[2]));
 
